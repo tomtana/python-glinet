14fc5c778a98d339a5115d380cd91ff2246ca545
Tomtana <python@fontana.onl>
2022-08-19 15:15:12 +0200

    change contact

--- docs/conf.py
+++ docs/conf.py
@@ -7,8 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'python-glinet'
-copyright = '2022, Thomas Fontana'
-author = 'Thomas Fontana'
+copyright = '2022, Tomtana'
+author = 'Tomtana'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ with open("LICENSE", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="python-glinet",
     version="0.4.0",
-    author="Thomas Fontana",
-    author_email="python@fontana.onl",
-    description="Python3 client for Gl.Inet LUCI API with firmware >=4.0",
+    author="Tomtana",
+    author_email="python@tomtana.net",
+    description="Python3 client for Gl.Inet LUCI API firmware 4.0 onwards.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url="https://github.com/tomtana/python-glinet",
