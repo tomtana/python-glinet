53705d95cdebea6b14288487526412a009278102
Tomtana <python@fontana.onl>
2022-08-22 12:48:48 +0200

    removed unused function

--- pyglinet/utils.py
+++ pyglinet/utils.py
@@ -2,27 +2,6 @@ from collections import namedtuple, OrderedDict
 import re
 
 
-def create_recursive_object(obj, obj_name):
-    if isinstance(obj, dict):
-        fields = sorted(obj.keys())
-        namedtuple_type = namedtuple(
-            typename=obj_name,
-            field_names=fields,
-            rename=True,
-        )
-        field_value_pairs = OrderedDict(
-            (str(field), create_recursive_object(obj[field], obj_name)) for field in fields)
-        try:
-            return namedtuple_type(**field_value_pairs)
-        except TypeError:
-            # Cannot create namedtuple instance so fallback to dict (invalid attribute names)
-            return dict(**field_value_pairs)
-    elif isinstance(obj, (list, set, tuple, frozenset)):
-        return [create_recursive_object(item, obj_name) for item in obj]
-    else:
-        return obj
-
-
 class ResultContainer(dict):
     def __init__(self, name, data):
         self.name = name
@@ -41,7 +20,6 @@ class ResultContainer(dict):
 
     def __str__(self):
         return str(self.__dict__)
-
 
 
 def sanitize_string(string):
